9eb612d26b4088e213490f5f6106081b9fcdaf1a
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-09-30 01:19:40 -0300

    multiple arquivos

--- arquivos.php
+++ arquivos.php
@@ -1,6 +1,6 @@
 <form method="POST" enctype="multipart/form-data" action = "recebedor.php">
 		Arquivo:
-		<input type="file" name ="arquivo"/> <br/><br/>
+		<input type="file" name ="arquivo[]" multiple/> <br/><br/>
 		<input type="submit" name = "enviar"/> <br/><br/>
 
 
--- recebedor.php
+++ recebedor.php
@@ -2,10 +2,20 @@
 
 	$arquivo= $_FILES['arquivo'];
 
-	if(isset($arquivo['tmp_name']) && empty($arquivo['tmp_name']) == false){
+	if(isset($_FILES['arquivo']) && empty($_FILES['arquivo']) == false){
+		if(count($_FILES['arquivo']['tmp_name']) > 0){
+
+			for($i = 0; $i < count($_FILES['arquivo']['tmp_name']) ; $i++ ){
+				$nomedoarquivo = md5($_FILES['arquivo']['name'][$q].time().rand(0,999).'.jpg');
+				move_uploaded_file($_FILES['arquivo']['name'],'/arquivos'.$nomedoarquivo); 
 
 
-		move_uploaded_file($arquivo['tmp_name'], $arquivo['name']);
+
+			}
+
+		}
+
+		
 	}
 
 

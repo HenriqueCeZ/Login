7cf367ddacdc45c4f14c0870f24d1cd9c7ece9cf
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-09-30 00:50:46 -0300

    Recebendo arquivos

---
+++ arquivos.php
@@ -0,0 +1,7 @@
+<form method="POST" enctype="multipart/form-data" action = "recebedor.php">
+		Arquivo:
+		<input type="file" name ="arquivo"/> <br/><br/>
+		<input type="submit" name = "enviar"/> <br/><br/>
+
+
+</form>
